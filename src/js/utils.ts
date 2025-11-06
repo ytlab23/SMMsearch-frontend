42ac09b8727429c470380b1e525a30de11ac1152
Bishal Biswas <bbiswas47@outlook.com>
2025-11-07 00:23:27 +0530

    # Fixes 1. Ignoring API Key Failed while deploying 2. Using Data from Saved Database instead of Real Time API Fetching. 3. Update Search Page. 4. Search query runs on Client Side

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -95,6 +95,20 @@ export function optimizeImageUrl(url:string, height = 100, format = 'webp') {
     return optimizedUrl;
 }
 
+export function getRandomElements(arr :any[], count:number) {
+    // Create a copy of the original array
+    let shuffled = arr.slice();
+
+    // Shuffle the copy
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+
+    // Return the first 'count' elements
+    return shuffled.slice(0, count);
+}
+
 export function getAPIPath() {
     let siteIs = "https://smm-admin-panel.vercel.app/";
     // let siteIs = "http://localhost:4321/";
